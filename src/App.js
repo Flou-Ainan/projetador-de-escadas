53d1e0b3def7f236899b7593fe763ee2b8d552cf
Flou-Ainan <flouainan@gmail.com>
2022-09-17 18:56:11 -0300

    Alteração de teste para netlify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ALTERÇÃO BEM  SUCEDIDA
         </a>
       </header>
     </div>
