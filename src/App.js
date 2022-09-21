e9e97bd8032bd873b5e85a9732f69dbcbb247a6b
Flou-Ainan <flouainan@gmail.com>
2022-09-20 22:17:53 -0300

    Entrada de dados e Usestate implementados @  bug no input do piso

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import './App.css';
-import Escada from './Escada';
+import './App.css'
+import Escada from './Escada'
+import Form from './Form'
+import React from 'react'
 
 function App() {
+    const [escada, setEscada] = React.useState(
+      {
+          altura: 280,
+          espelho: 17,
+          piso: 28
+      }
+    )
+    
+    function handleChange(event) {
+      const {id, value} = event.target
+      setEscada(prevEscada => {
+          return {
+              ...prevEscada,
+              [id]: value
+          }
+      })    
+    }
+  
     return (
         <div className="App">
           <h2>Projetador de Escadas</h2>
-          <Escada />
+          <Form 
+            altura={escada.altura}
+            espelho={escada.espelho}
+            piso={escada.piso}
+            handleChange={handleChange}
+          />
+          <br></br>
+          <hr></hr>
+          <Escada
+            altura={escada.altura}
+            espelho={escada.espelho}
+            piso={escada.piso}          
+          />
         </div>
     );
 }
 
-export default App;
+export default App
--- src/Escada.js
+++ src/Escada.js
@@ -2,7 +2,80 @@ import './App.css';
 import { useEffect, useRef} from 'react';
 import p5 from 'p5';
 
-function Escada() {
+function Escada(props) {
+    
+    function sketch(p) {
+        p.setup = function() {
+            //Dados de Inicialização
+            let alturaCM = props.altura
+            let espelhoCM = props.espelho
+            let pisoCM = props.piso
+            let baseCM = 0
+            let degQuant = 0
+
+            let alturaCanvasPX = 400
+            let larguraCanvas = 600
+
+
+    
+    
+            //Cria o Canvas
+            p.createCanvas(larguraCanvas, alturaCanvasPX);
+            p.background(200);
+    
+            // Set colors
+            p.fill(100, 100, 100);
+            p.stroke(127, 63, 120, 0);
+    
+            // Gerador da escada
+            let alturaDeg = alturaCM - espelhoCM 
+            let distanciaDeg = 0
+            // Ultimo Degrau do andar superior
+            p.rect(
+                0,
+                alturaCanvasPX-alturaCM,
+                2,
+                espelhoCM
+            )
+    
+            //  --- Montagem da escada ---- //
+    
+            // Projeto de Objeto escada como estrutura de dados
+            // Projeto de loop que varre o objeto escada
+            //let i = 0;
+            /* while(i < escada.degraus.length){
+                let degrau = escada.degraus[i]
+                console.log(
+                    degrau.altura + 
+                    degrau.piso + 
+                    degrau.espelho
+                    )
+            } */
+    
+            while(alturaDeg >= 1){
+                p.rect(
+                    distanciaDeg,  //Distancia em relação a parede
+                    alturaCanvasPX-alturaDeg, // Posição Y do degrau
+                    pisoCM, //tamanho do espelho do degrau 
+                    alturaDeg // altura do degrau 
+                )
+                alturaDeg -= espelhoCM
+                distanciaDeg += pisoCM
+                degQuant += 1
+                baseCM += pisoCM 
+    
+            }
+            degQuant += 1
+            console.log(degQuant)
+            console.log(baseCM)
+        }
+    }
+    
+    
+    
+    
+    
+    
     // create a reference to the container in which the p5 instance should place the canvas
     const p5ContainerRef = useRef();
     useEffect(() => {
@@ -13,33 +86,11 @@ function Escada() {
         return () => {
             p5Instance.remove();
         }
-    }, []);
+    });
 
     return (
         <div className="Escada" ref={p5ContainerRef} />
     );
-}
-
-
-
-function sketch(p) {
-  // p is a reference to the p5 instance this sketch is attached to
-  p.setup = function() {
-    p.createCanvas(720, 400);
-    p.background(170);
-  
-    // Set colors
-    p.fill(204, 101, 192, 127);
-    p.stroke(127, 63, 120);
-  
-    // A rectangle
-    p.rect(40, 120, 120, 40);
-  
-
-  p.draw = function() {
-      // your draw code here
-  }
-}
 }
 
 
